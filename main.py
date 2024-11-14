953e79fdc7757d4faa6c4e621bf0352c3bca7f10
OvidioCalvet <calvetovidio@gmail.com>
2024-11-14 10:25:18 -0500

    wip: created a for loop that instantiates a new object for each question and adds it to a question list

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from question_model import Question
 from data import question_data
+
+question: list = []
+
+for i in range(0, len(question_data)):
+
+    q = Question(text=question_data[i]['text'], answer= question_data[i]['answer'])
+
+    question.append(q)
+
+
